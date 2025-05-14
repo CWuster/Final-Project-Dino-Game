9f76ce0d6db69928c4a2bc3d9edecffff9d038ea
JayPankajPatel <jayp102501@gmail.com>
2025-05-13 23:28:26 -0700

    kindve working sprites

--- py/main.c
+++ py/main.c
@@ -1,3 +1,4 @@
+#include "./Generated_Code/modified_cactus.h"
 #include "./Generated_Code/modified_dino.h"
 #include <assert.h>
 #include <errno.h>
@@ -18,6 +19,23 @@ int main(int argc, char *argv[]) {
     printf("{");
     for (int i = 0; i < n; ++i) {
       if (modified_dino_rom[j][i] == 0)
+        printf(" ");
+      else
+        printf("*");
+    }
+    printf("},");
+    printf("\n");
+  }
+  int m =
+      ((int)sizeof(modified_cactus_rom) / (int)sizeof(modified_dino_rom[0]));
+  int n = ((int)sizeof(modified_cactus_rom[0]) /
+           (int)sizeof(modified_cactus_rom[0][0]));
+  printf("%d\n", m);
+  printf("%d\n", n);
+  for (int j = 0; j < m; ++j) {
+    printf("{");
+    for (int i = 0; i < n; ++i) {
+      if (modified_cactus_rom[j][i] == 0)
         printf(" ");
       else
         printf("*");
